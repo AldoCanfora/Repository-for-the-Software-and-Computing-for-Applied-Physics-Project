f21670b2fe8d6fb0540cd259783c93ad6c34b431
AldoCanfora <aldo.canfora@users.noreply.github.com>
2023-10-04 17:33:16 +0200

    add docstring in testing.py

--- testing.py
+++ testing.py
@@ -14,6 +14,27 @@ def seed_value():
     return 1  
 
 def test_initial_state_grid(width,height):
+    """
+Test the initialization of the cellular automaton grid.
+
+This test function checks whether the cellular automaton grid is properly initialized with the specified dimensions
+and contains only values of 0 and 1.
+
+Args:
+    width (int): The width of the grid.
+    height (int): The height of the grid.
+
+Test Steps:
+1. Initializes the cellular automaton grid with the specified dimensions, where all values are either 0 or 1.
+2. Checks if the dimensions of the grid match the provided width and height.
+3. Verifies that all values in the grid are either 0 or 1.
+
+Raises:
+    AssertionError: If any of the test conditions fail.
+
+Example Usage:
+    test_initial_state_grid(10, 20)  # Test with a 10x20 grid.
+    """
     #Initialazing the model with width*height grid of values 0 and 1."
     model = cellular_automata.initial_state_grid(height,width) 
     #Test if the dimensions of the grid are width and height."
@@ -32,16 +53,93 @@ def grid():
     ]
 
 def test_count_neighbors_center(grid):
+    """
+Test the counting of neighbors for a cell at the center of the grid.
+
+This test function verifies that the 'count_neighbors' function correctly counts the number of neighboring cells
+for a cell located at the center of the grid.
+
+Args:
+    grid (list[list[int]]): A 2D grid representing the cellular automaton.
+
+Test Steps:
+1. Calls the 'count_neighbors' function with the provided 'grid', in above grid() function, and coordinates (1, 1) 
+   representing the center cell.
+2. Asserts that the function returns the expected count of neighbors for the center cell.
+
+Raises:
+    AssertionError: If the count of neighbors for the center cell is not equal to the expected count.
+
+Example Usage:
+    grid = [
+        [0, 0, 1],
+        [1, 1, 0],
+        [0, 1, 0]
+    ]
+    test_count_neighbors_center(grid)  # Expected result is 3.
+    """
     assert cellular_automata.count_neighbors(grid, 1, 1) == 3
 
 def test_count_neighbors_corner(grid):
+    """
+Test the counting of neighbors for a cell at the corner of the grid.
+
+This test function verifies that the 'count_neighbors' function correctly counts the number of neighboring cells
+for a cell located at the corner of the grid.
+
+Args:
+    grid (list[list[int]]): A 2D grid representing the cellular automaton.
+
+Test Steps:
+1. Calls the 'count_neighbors' function with the provided 'grid', in above grid() function, and coordinates (0, 0) 
+   representing the center cell.
+2. Asserts that the function returns the expected count of neighbors for the corner cell.
+
+Raises:
+    AssertionError: If the count of neighbors for the corner cell is not equal to the expected count.
+    """
     assert cellular_automata.count_neighbors(grid, 0, 0) == 2
 
 def test_count_neighbors_edge(grid):
+    """
+Test the counting of neighbors for a cell at the edge of the grid.
+
+This test function verifies that the 'count_neighbors' function correctly counts the number of neighboring cells
+for a cell located at the edge of the grid.
+
+Args:
+    grid (list[list[int]]): A 2D grid representing the cellular automaton.
+
+Test Steps:
+1. Calls the 'count_neighbors' function with the provided 'grid', in above grid() function, and coordinates (0, 1) 
+   representing the center cell.
+2. Asserts that the function returns the expected count of neighbors for the edge cell.
+
+Raises:
+    AssertionError: If the count of neighbors for the edge cell is not equal to the expected count.
+    """    
     assert cellular_automata.count_neighbors(grid, 0, 1) == 3
     
 
-def test_update_cell():
+def test_update_cell_death_to_live():
+    """
+Test the update of a cell in the cellular automaton grid.
+
+This test function verifies the update of a cell in the grid based on the rules of the cellular automaton Conway's game of life.
+
+Test Steps:
+1. Initializes a grid with specific values, where 1 represents a live cell, and 0 represents a dead cell.
+2. Calls the 'update_cell' function with the provided 'grid' and coordinates (1, 1) to update the center cell.
+3. Asserts that the function returns the expected state of the updated cell.
+
+Note:
+- In this test case, we test the scenario where a cell is initially dead (0) and has 3 neighboring live cells (1). 
+According to the rules of the cellular automaton Conway's game of life, such a cell should become alive (1) in 
+the next generation.
+
+Raises:
+    AssertionError: If the updated state of the cell is not equal to the expected state.
+    """
     # Test 1 death cell and 3 near live cells
     grid = [
         [1, 0, 0],
@@ -50,6 +148,25 @@ def test_update_cell():
     ]
     assert cellular_automata.update_cell(grid, 1, 1) == 1
 
+def test_update_cell_live_to_death():
+    """
+Test the update of a cell in the cellular automaton grid.
+
+This test function verifies the update of a cell in the grid based on the rules of the cellular automaton Conway's game of life.
+
+Test Steps:
+1. Initializes a grid with specific values, where 1 represents a live cell, and 0 represents a dead cell.
+2. Calls the 'update_cell' function with the provided 'grid' and coordinates (1, 1) to update the center cell.
+3. Asserts that the function returns the expected state of the updated cell.
+
+Note:
+- In this test case, we test the scenario where a cell is initially live (1) and has no neighbors, so all death cells near(0). 
+According to the rules of the cellular automaton Conway's game of life, such a cell should become death (0) in 
+the next generation.
+
+Raises:
+    AssertionError: If the updated state of the cell is not equal to the expected state.
+    """
     # Test 1 live cell and no near live cells
     grid = [
         [0, 0, 0],
@@ -58,6 +175,25 @@ def test_update_cell():
     ]
     assert cellular_automata.update_cell(grid, 1, 1) == 0
 
+def test_update_cell_live_with_one_neighbor():
+    """
+Test the update of a cell in the cellular automaton grid.
+
+This test function verifies the update of a cell in the grid based on the rules of the cellular automaton Conway's game of life.
+
+Test Steps:
+1. Initializes a grid with specific values, where 1 represents a live cell, and 0 represents a dead cell.
+2. Calls the 'update_cell' function with the provided 'grid' and coordinates (1, 1) to update the center cell.
+3. Asserts that the function returns the expected state of the updated cell.
+
+Note:
+- In this test case, we test the scenario where a cell is initially live (1) and has 1 neighbor live cells (1). 
+According to the rules of the cellular automaton Conway's game of life, such a cell should become death (0) in 
+the next generation.
+
+Raises:
+    AssertionError: If the updated state of the cell is not equal to the expected state.
+    """
     # Test 1 live cell and 1 near live cell
     grid = [
         [0, 1, 0],
@@ -66,6 +202,25 @@ def test_update_cell():
     ]
     assert cellular_automata.update_cell(grid, 1, 1) == 0
 
+def test_update_cell_live_with_4_neighbors():
+    """
+Test the update of a cell in the cellular automaton grid.
+
+This test function verifies the update of a cell in the grid based on the rules of the cellular automaton Conway's game of life.
+
+Test Steps:
+1. Initializes a grid with specific values, where 1 represents a live cell, and 0 represents a dead cell.
+2. Calls the 'update_cell' function with the provided 'grid' and coordinates (1, 1) to update the center cell.
+3. Asserts that the function returns the expected state of the updated cell.
+
+Note:
+- In this test case, we test the scenario where a cell is initially live (1) and has 4 neighboring live cells (1). 
+According to the rules of the cellular automaton Conway's game of life, such a cell should become death (0) in 
+the next generation.
+
+Raises:
+    AssertionError: If the updated state of the cell is not equal to the expected state.
+    """
     # Test 1 live cell and 4 near live cells
     grid = [
         [1, 1, 1],
@@ -74,6 +229,26 @@ def test_update_cell():
     ]
     assert cellular_automata.update_cell(grid, 1, 1) == 0
 
+
+def test_update_cell_live_with_2_neighbors():
+    """
+Test the update of a cell in the cellular automaton grid.
+
+This test function verifies the update of a cell in the grid based on the rules of the cellular automaton Conway's game of life.
+
+Test Steps:
+1. Initializes a grid with specific values, where 1 represents a live cell, and 0 represents a dead cell.
+2. Calls the 'update_cell' function with the provided 'grid' and coordinates (1, 1) to update the center cell.
+3. Asserts that the function returns the expected state of the updated cell.
+
+Note:
+- In this test case, we test the scenario where a cell is initially live (1) and has 2 neighboring live cells (1). 
+According to the rules of the cellular automaton Conway's game of life, such a cell should stay alive (1) in 
+the next generation.
+
+Raises:
+    AssertionError: If the updated state of the cell is not equal to the expected state.
+    """
     # Test 1 live cell and 2 near live cells
     grid = [
         [0, 1, 1],
@@ -82,6 +257,25 @@ def test_update_cell():
     ]
     assert cellular_automata.update_cell(grid, 1, 1) == 1
 
+def test_update_cell_death_with_4_neighbors():
+    """
+Test the update of a cell in the cellular automaton grid.
+
+This test function verifies the update of a cell in the grid based on the rules of the cellular automaton Conway's game of life.
+
+Test Steps:
+1. Initializes a grid with specific values, where 1 represents a live cell, and 0 represents a dead cell.
+2. Calls the 'update_cell' function with the provided 'grid' and coordinates (1, 1) to update the center cell.
+3. Asserts that the function returns the expected state of the updated cell.
+
+Note:
+- In this test case, we test the scenario where a cell is initially dead (0) and has 4 neighboring live cells (1). 
+According to the rules of the cellular automaton Conway's game of life, such a cell should stay death (0) in 
+the next generation.
+
+Raises:
+    AssertionError: If the updated state of the cell is not equal to the expected state.
+    """
     # Test 1 death cell with 4 near live cells
     grid = [
         [1, 1, 1],
@@ -90,6 +284,25 @@ def test_update_cell():
     ]
     assert cellular_automata.update_cell(grid, 1, 1) == 0
 
+def test_update_cell_death_with_3_neighbors():
+    """
+Test the update of a cell in the cellular automaton grid.
+
+This test function verifies the update of a cell in the grid based on the rules of the cellular automaton Conway's game of life.
+
+Test Steps:
+1. Initializes a grid with specific values, where 1 represents a live cell, and 0 represents a dead cell.
+2. Calls the 'update_cell' function with the provided 'grid' and coordinates (1, 1) to update the center cell.
+3. Asserts that the function returns the expected state of the updated cell.
+
+Note:
+- In this test case, we test the scenario where a cell is initially dead (0) and has 3 neighboring live cells (1). 
+According to the rules of the cellular automaton Conway's game of life, such a cell should become alive (1) in 
+the next generation. This is the same case of test_update_cell_death_to_live but with a different order of neighbors.
+
+Raises:
+    AssertionError: If the updated state of the cell is not equal to the expected state.
+    """
     # Test 1 death cell with 3 near live cells
     grid = [
         [1, 1, 0],
