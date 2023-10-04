1057203ee5bbbfa8c082ebd1058056d6c87f919e
AldoCanfora <aldo.canfora@users.noreply.github.com>
2023-10-04 17:07:03 +0200

    remove dependecies from cellular_automata.py from cellular_automata_visualization.py

--- cellular_automata_visualization.py
+++ cellular_automata_visualization.py
@@ -1,8 +1,12 @@
 import cellular_automata
 import pygame
+import parameters
+
+# Initialize pygame and window
+screen = pygame.display.set_mode((parameters.WIDTH, parameters.HEIGHT))
 
 # create initial grid
-grid = cellular_automata.initial_state_grid(cellular_automata.WIDTH//10, cellular_automata.HEIGHT//10)
+grid = cellular_automata.initial_state_grid(parameters.WIDTH//10, parameters.HEIGHT//10)
 
 # Game loop
 running = True
@@ -15,7 +19,7 @@ while running:
     for i, row in enumerate(grid):
         for j, cell in enumerate(row):
             color = (255, 255, 255) if cell == 1 else (0, 0, 0)
-            pygame.draw.rect(cellular_automata.screen, color, (j*10, i*10, 10, 10))
+            pygame.draw.rect(screen, color, (j*10, i*10, 10, 10))
 
     # update grid
     grid = cellular_automata.update_grid(grid)
