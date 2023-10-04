ddd77e6762fd1db784dddbb6513eed58df9f9f53
AldoCanfora <aldo.canfora@users.noreply.github.com>
2023-10-04 18:44:44 +0200

    add reading of parameters from configuration file in celliular_automata_visualization.py

--- cellular_automata_visualization.py
+++ cellular_automata_visualization.py
@@ -1,6 +1,54 @@
 import cellular_automata
 import pygame
-import parameters
+
+# Prompt the user to enter the filename
+if __name__ == "__main__":
+    """
+    This script reads a configuration file specified by the user, parses it line by line, 
+    and stores the configuration values in a dictionary. It assumes that the configuration file contains key-value 
+    pairs separated by '='. The keys are strings, and the values are expected to be integers.
+
+    Usage:
+    1. Run the script and provide the configuration file's filename when prompted.
+    2. The script will attempt to open and read the file, extract key-value pairs, 
+    and store them in the 'config_values' dictionary.
+    3. If the specified file is not found, a 'FileNotFoundError' will be raised, and an error message will be displayed.
+    4. If any other error occurs during file reading or parsing, 
+    an error message will be displayed with details about the exception.
+
+    Example Configuration File Format:
+    -------------------
+    key1=42
+    key2=123
+    key3=987
+    -------------------
+
+    Note:
+    - Ensure that the configuration file is in the specified format, with one key-value pair per line.
+    - The values are assumed to be integers, so any non-integer values will raise an error.
+
+    Author: Aldo Canfora
+    Date: 04/10/2023
+    """
+    filename = input("Enter configuration filename: ")
+
+    config_values = {}
+    try:
+        with open(filename, 'r') as config_file:
+            for line in config_file:
+                key, value = line.strip().split('=')
+                config_values[key] = int(value)
+    except FileNotFoundError:
+        print(f"The file '{filename}' was not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+    WIDTH = config_values.get('WIDTH') 
+    HEIGHT = config_values.get('HEIGHT')
+    seed_value = config_values.get('seed_value')
+
+
+
 
 """
 This script demonstrates a simple cellular automaton simulation using pygame for visualization.
@@ -18,10 +66,10 @@ Requirements:
 """
 
 # Initialize pygame and window
-screen = pygame.display.set_mode((parameters.WIDTH, parameters.HEIGHT))
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 # create initial grid
-grid = cellular_automata.initial_state_grid(parameters.WIDTH//10, parameters.HEIGHT//10)
+grid = cellular_automata.initial_state_grid(WIDTH//10, HEIGHT//10, seed_value)
 
 # Game loop
 running = True
@@ -44,3 +92,6 @@ while running:
 
 # clean and quit pygame
 pygame.quit()
+
+
+
