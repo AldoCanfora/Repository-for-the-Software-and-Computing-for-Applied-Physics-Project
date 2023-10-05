461a12fecb942753ef6ceeba51bd159087f7b5b3
AldoCanfora <aldo.canfora@users.noreply.github.com>
2023-10-06 01:45:13 +0200

    add functions for update cell with different configurations of borders: death,alive,symmetric and circular

--- cellular_automata.py
+++ cellular_automata.py
@@ -92,9 +92,9 @@ def update_cell(grid, x, y):
     else: # considering death cells
         return 1 if count == 3 else 0
 
-def update_grid(grid):
+def update_grid_death_borders(grid):
     """
-    Update the entire binary grid based on Conway's Game of Life rules.
+    Update the entire binary grid based on Conway's Game of Life rules with death borders.
 
     Parameters:
         grid (list): The binary grid representing the current state.
@@ -111,3 +111,131 @@ def update_grid(grid):
         updated_grid = update_grid(initial_grid)
     """
     return [[update_cell(grid, i, j) for j in range(len(grid[0]))] for i in range(len(grid))]
+
+
+def update_grid_circular_borders(grid):
+    """
+    Update the entire binary grid based on Conway's Game of Life rules with wraparound borders.
+
+    Parameters:
+        grid (list): The binary grid representing the current state.
+
+    Returns:
+        list: A new binary grid representing the updated state based on the rules.
+
+    Note:
+        This function applies the rules of Conway's Game of Life to each cell in the input grid
+        and generates a new grid as the next state with wraparound borders.
+
+    Examples:
+        Given an initial grid, you can use this function to obtain the next generation grid.
+        updated_grid = update_grid(initial_grid)
+    """
+    num_rows = len(grid)
+    num_cols = len(grid[0])
+    
+    # Create an extended grid with wraparound borders
+    extended_grid = [[grid[i][j] for j in range(num_cols)] for i in range(num_rows)]
+    
+    # Extend rows to wraparound
+    extended_grid.insert(0, extended_grid[-1])
+    extended_grid.append(extended_grid[1])
+    
+    # Extend columns to wraparound
+    for row in extended_grid:
+        row.insert(0, row[-1])
+        row.append(row[1])
+    
+    # Calculate the updated state based on the rules
+    new_grid = [[update_cell(extended_grid, i, j) for j in range(1, num_cols + 1)] for i in range(1, num_rows + 1)]
+    
+    return new_grid
+
+
+def update_grid_symmetric_borders(grid):
+    """
+    Update the entire binary grid based on Conway's Game of Life rules with symmetric borders.
+
+    Parameters:
+        grid (list): The binary grid representing the current state.
+
+    Returns:
+        list: A new binary grid representing the updated state based on the rules with symmetric borders.
+
+    Note:
+        This function applies the rules of Conway's Game of Life to each cell in the input grid
+        and generates a new grid as the next state with symmetric borders.
+
+    Examples:
+        Given an initial grid, you can use this function to obtain the next generation grid.
+        updated_grid = update_grid_symmetric_borders(initial_grid)
+    """
+    num_rows = len(grid)
+    num_cols = len(grid[0])
+
+    # Calculate the updated state based on the rules
+    new_grid = [[update_cell(grid, i, j) for j in range(num_cols)] for i in range(num_rows)]
+
+    # Set border cells to match their symmetric counterparts
+    for i in range(num_rows):
+        for j in range(num_cols):
+            if i == 0:
+                # Top border
+                new_i = num_rows - 1
+            elif i == num_rows - 1:
+                # Bottom border
+                new_i = 0
+            else:
+                new_i = i
+
+            if j == 0:
+                # Left border
+                new_j = num_cols - 1
+            elif j == num_cols - 1:
+                # Right border
+                new_j = 0
+            else:
+                new_j = j
+
+            new_grid[i][j] = new_grid[new_i][new_j]
+
+    return new_grid
+
+
+
+
+
+def update_grid_alive_borders(grid):
+    """
+    Update the entire binary grid based on Conway's Game of Life rules with all alive borders.
+
+    Parameters:
+        grid (list): The binary grid representing the current state.
+
+    Returns:
+        list: A new binary grid representing the updated state based on the rules with all alive borders.
+
+    Note:
+        This function applies the rules of Conway's Game of Life to each cell in the input grid
+        and generates a new grid as the next state with all alive borders.
+
+    Examples:
+        Given an initial grid, you can use this function to obtain the next generation grid.
+        updated_grid = update_grid_all_alive(initial_grid)
+    """
+    num_rows = len(grid)
+    num_cols = len(grid[0])
+
+    # Calculate the updated state based on the rules
+    new_grid = [[update_cell(grid, i, j) for j in range(num_cols)] for i in range(num_rows)]
+
+    # Set all border cells to alive
+    for i in range(num_rows):
+        new_grid[i][0] = 1
+        new_grid[i][num_cols - 1] = 1
+
+    for j in range(num_cols):
+        new_grid[0][j] = 1
+        new_grid[num_rows - 1][j] = 1
+
+    return new_grid
